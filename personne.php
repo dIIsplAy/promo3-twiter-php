60e96e23e8f24ba0c7f098ad3ef9ac6566f6a515
cristophol <thomas.cristophol@gmail.com>
2017-06-13 11:37:04 +0200

    last committ

--- personne.php
+++ personne.php
@@ -1,31 +1,61 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of personne
- *
- * @author cristophol
- */
+
+
+    
 class Personne {
    private $nom;
    private $prenom;
    private $age;
    private $region;
-    
- public function __toString() {
-        return 'mon nom est'. $this->nom . ',ma race est'. $this->prenom . ',ma couleur est'. $this->age . ',je suis née le'. $this->region;
-    } 
-     public function __construct($nom, $prenom, $region, $age) {
+   
+   
+   
+   public function __construct($nom, $prenom, $region, $age) {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->region = $region;
         $this->age = $age;
         
-     }    
+     }  
+   
+   function getNom() {
+       return $this->nom;
+   }
+
+   function getPrenom() {
+       return $this->prenom;
+   }
+
+   function getAge() {
+       return $this->age;
+   }
+
+   function getRegion() {
+       return $this->region;
+   }
+
+   function setNom($nom) {
+       $this->nom = $nom;
+   }
+
+   function setPrenom($prenom) {
+       $this->prenom = $prenom;
+   }
+
+   function setAge($age) {
+       $this->age = $age;
+   }
+
+   function setRegion($region) {
+       $this->region = $region;
+   }
+
+       
+ /*public function __toString() {
+        return 'mon nom est'. $this->nom . ',ma prenom est'. $this->prenom . ',mon age est'. $this->age . ',ma region'. $this->region;
+    } */
+     
     
 }
